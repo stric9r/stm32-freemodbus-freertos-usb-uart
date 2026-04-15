edd593391706596b9fb89ae73346eda81b9663f4
Stric Roberts <stric9r@gmail.com>
2026-04-15 16:37:35 -0400

    Enable assert, and debug connected check

--- Src/main.c
+++ Src/main.c
@@ -17,14 +17,14 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+#include <dma.h>
+#include <gpio.h>
+#include <icache.h>
+#include <lptim.h>
+#include <usart.h>
+#include <usb.h>
 #include "main.h"
 #include "cmsis_os.h"
-#include "dma.h"
-#include "icache.h"
-#include "lptim.h"
-#include "usart.h"
-#include "usb.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -212,6 +212,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE END Callback 1 */
 }
 
+
+bool isDebuggerAttached(void) {
+    return (CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk) != 0;
+}
+
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -236,9 +241,11 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	// @todo [2026-4-12] Print to a serial debug port
+
+	if(isDebuggerAttached())
+	{
+		Error_Handler();
+	}
 }
 #endif /* USE_FULL_ASSERT */
